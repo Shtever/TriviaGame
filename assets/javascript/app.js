befb58a226913f97ed8d7ce3eac57706b456ad92
Stephen McMillen <Shtevetm@gmail.com>
2019-09-16 22:02:34 -0500

    scrapped entire html & js to begin from scratch. Took questions & answers from html and made them objects in JS. Going to be a long night

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,43 @@
-// Begin Timer
-var score = 0;
-//Questions
 
-$("#submit").on("click", function (){
-for(var i = 1; i <= 10; i++) {
-    radios = document.getElementsByName("q" + i);
-      if(radio.value == "correct") {
-        score++;
-        console.log(score);
-        $("#score").text(score);
-      }
-    }
-})
+    var triviaQuestions = [{
+        question:"Tupac Shakur rapped that he wanted to spend the afterlife in this locale",
+        answers:["Thugz Mansion", "Gangsterz Grotto", "Hustler Heaven", "Pimp's Paradise"],
+        correct: "Thugz Mansion",
+    },  {
+        question:"2. In 1993, Snoop Dogg spent his time laid back with this combination:",
+        answers:["Guns & Drugs", "Gin & Juice", "Homework & Volunteering", "Hookers & Blow"],
+        correct: "Gin & Juice",
+    },  {
+        question:"3. What NY borough did the Wu-Tang Clan refer to as 'Shaolin'?",
+        answers:["Brooklyn", "Queens", "Staten Island", "The Bronx"],
+        correct: "Staten Island",
+    },  {
+        question:"4. 'Havoc' & 'Prodigy' were the stage names of this duo who hailed from Queens.",
+        answers:["De La Soul", "M.O.P", "Gang Starr", "Mobb Deep",],
+        correct: "Mobb Deep",
+    },  {
+        question:"5. Which Beastie Boys music video featured a giant robot destroying a model city?",
+        answers:["Sabotage", "Brass Monkey", "No Sleep Till Brooklyn", "Intergalactic"],
+        correct: "Intergalactic",
+    },  {
+        question:"Naughty By Nature hailed from THIS city:",
+        answers: ["East Orange, NJ", "Brooklyn, NY", "Los Angeles, CA", "Detroit, MI"],
+        correct: "East Orange, NJ",
+    },  {
+        question:"7. 'Mista Dobalina' by THIS artist peaked at #6 on the Billboard charts in 1991.",
+        answers:["A Tribe Called Quest", "Ice Cube", "Del tha Funkee Homosapien", "Public Enemy"],
+        correct: "Del tha Funkee Homosapien",
+    },  {
+        question:"8. Notorious B.I.G. claimed he used to read 'Word Up!' magazine with who his limo?",
+        answers:["Funkmaster Flex & Dr. Dre", "Salt-n-Pepa & Heavy D", "Nas & Lil Kim", "Public Enemy"],
+        correct: "Salt-n-Pepa & Heavy D",
+    },  {
+        question:"9. Which group put out the track 'Tootsie Roll' in 1994?",
+        answers:["Ghost Town DJ's", "Tag Team", "69 Boyz", "The Luniz"],
+        correct: "69 Boyz",
+    },  {
+        question:"10. Who was NOT a member of the rap group NWA?",
+        answers:["Method Man", "Eazy-E", "DJ Yella", "Ice Cube"],
+        correct: "Method Man",
+    },  
+]
